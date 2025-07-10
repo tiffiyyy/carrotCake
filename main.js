41b3a6a5488f1d6827fe2d9b7d4c265fe8a8829f
tiffiyyy <Tiffany.Le710@outlook.com>
2025-07-09 23:30:28 -0700

    updated styling (ex. background color, window sizing, position) and got buttons to work

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 375,
+    height: 612,
+    resizable: false,
   })
-
+  // win.setAspectRatio(1);
   win.loadFile('index.html')
 }
 
@@ -19,18 +20,6 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
-
-/*
-function goToScreen(screenId) {
-  const screens = document.querySelectorAll('.screen');
-  screens.forEach(screen => {
-    screen.classList.remove('active');
-  });
-
-  const target = document.getElementById(screenId);
-  if (target) target.classList.add('active');
-}
-*/
 
 function goToScreen(screenId) {
   // Hide all screens
